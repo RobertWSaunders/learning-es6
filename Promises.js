4c49a03f019a28b98fe1929d32ab6b5cd6610e3e
Robert Saunders <robert.saunders@outlook.com>
2017-05-05 15:29:13 -0400

    changes to the promises

--- Promises.js
+++ Promises.js
@@ -3,5 +3,9 @@
 //we use promises to solve long running processes or requests
 //three states of promises
 //unresolved - waiting for something to finish
-//resolved - something finished and it was ok
-//rejected - something finished and went bad
+//resolved - something finished and it was ok, callback 'then'
+//rejected - something finished and went bad, callback 'catch'
+
+//mostly use promises for AJAX requests
+//promises are native to JS now
+const promise = new Promise();
