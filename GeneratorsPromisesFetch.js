3b242b751ac9bcd594bb10ed91934291b6db67cc
Robert Saunders <robert.saunders@ibm.com>
2017-05-05 15:15:32 -0400

    worked on generators and added a file to implement promises

---
+++ GeneratorsPromisesFetch.js
@@ -0,0 +1,72 @@
+//for of loop
+const colors = ['red', 'green', 'blue'];
+
+for (let color of colors) {
+  console.log(color);
+}
+
+//generators
+//a generator is a function that can be entered and exited multiple times
+//the asterisk indicates a generator
+function *shopping() {
+  //stuff on the sidewalk
+
+  //walking down the sidewalk
+
+  //go into the store with cash
+  //yield is a keyword added in ES6
+  const stuffFromStore = yield 'cash';
+  //walking to laundry
+  const stuffFromLaundry = yield 'laundry';
+
+  return [stuffFromStore, stuffFromLaundry];
+}
+
+//stuff in the store
+const gen = shopping();
+//code executes here
+gen.next();//leaving our house
+//walking into the store
+//walking up and down aisles
+//purchase our stuff
+gen.next('groceries');//leaving the store with groceries
+gen.next('clean cloths');
+
+
+function *colors() {
+  yield 'red';
+  yield 'blue';
+  yield 'green';
+}
+
+const gen = colors();
+gen.next();
+gen.next();
+gen.next();
+gen.next();
+
+//can use generators to iterate through any type of object
+const myColors = [];
+for (let color of colors()) {
+  myColor.push(color);
+}
+myColors;
+
+const engineeringTeam = {
+  size: 3,
+  department: 'Engineering',
+  lead: 'Jill',
+  manager : 'Alex',
+  engineer: 'Dave'
+}
+
+function *TeamIterator(team) {
+  yield team.lead;
+  yield team.manager;
+  yield team.engineer;
+}
+
+const names = [];
+for (let name of TeamIterator(engineeringTeam)) {
+  names.push(name);
+}
---
+++ Promises.js
@@ -0,0 +1,7 @@
+//promises
+//there is no concept of sleeping/delay in javascript
+//we use promises to solve long running processes or requests
+//three states of promises
+//unresolved - waiting for something to finish
+//resolved - something finished and it was ok
+//rejected - something finished and went bad
